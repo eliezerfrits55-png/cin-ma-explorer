f86bf69627a8fc8a93adb2e2109209cac323069c
eliezer <eliezerfrits55@gmail.com>
2026-04-21 15:20:34 +0200

    Enhance MovieCard with additional movie details
    Updated the MovieCard component to improve the display of movie details and added conditional badges for trending, top-rated, and classic movies.

--- MovieCard.jsx
+++ MovieCard.jsx
@@ -1,13 +1,21 @@
+
+//wara ntem fridz eliezer
+
+//afficher un film individuel
 function MovieCard({ movie }) {
-  return (
-    <div className='movie-card'>
-      <h3>{movie.title}</h3>
-      <p>Genre: {movie.genre}</p>
-      <p>Year: {movie.year}</p>
-      <p>Rating: {movie.rating}</p>
-      {movie.isTrending && <span>Trending</span>}
-    </div>
-  );
+    return (
+        <div className="movie-card">
+            <h3>{movie.title}</h3>  
+            <p>Genre: {movie.genre}</p>
+            <p>Annee: {movie.year}</p>
+            <p>Note: {movie.rating}/10</p>
+
+            {/**logique conditionnelle */}
+            {movie.isTrending && <span className="badge trending">Trending</span>}
+            {movie.rating >8 && <span className="badge top-rated">Top Rated</span>}
+            {movie.year<2000 && <span className="badge classic">Classic</span>}
+        </div>
+    );
 }
 
 export default MovieCard;
